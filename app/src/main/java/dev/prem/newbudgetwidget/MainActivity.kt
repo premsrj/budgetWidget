9b335870c405649a59d8281156d5680414a64521
Premsuraj <premsrj@gmail.com>
2023-05-11 17:59:14 +0530

    Test commit

--- app/src/main/java/dev/prem/newbudgetwidget/MainActivity.kt
+++ app/src/main/java/dev/prem/newbudgetwidget/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                    .setAction("Ok", null).show()
         }
     }
 

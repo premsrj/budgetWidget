a3876c77f0db7118facdafee9c94cea26e702678
Premsuraj <premsrj@gmail.com>
2023-05-12 16:17:55 +0530

    Developed UI of first fragment that gives an overview

--- app/src/main/java/dev/prem/newbudgetwidget/FirstFragment.kt
+++ app/src/main/java/dev/prem/newbudgetwidget/FirstFragment.kt
@@ -1,11 +1,32 @@
 package dev.prem.newbudgetwidget
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
 import dev.prem.newbudgetwidget.databinding.FragmentFirstBinding
 
 /**
@@ -24,7 +45,84 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        var listItems = ArrayList<String>(2)
+        listItems.add("First")
+        listItems.add("Second")
+        _binding = FragmentFirstBinding.inflate(inflater, container, false).apply {
+            composeView.setContent {
+                // You're in Compose world!
+                MaterialTheme {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    ) {
+                        Text(
+                            text = "Today",
+                            color = colorResource(R.color.textColor),
+                            textAlign = TextAlign.Center,
+                        )
+                        Card(
+                            backgroundColor = colorResource(R.color.cardBackground),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                Text(
+                                    text = "411.73 left",
+                                    color = colorResource(R.color.textColor),
+                                    fontSize = 22.sp
+                                )
+                                Text(
+                                    text = "411.73 limit",
+                                    color = colorResource(R.color.textColor),
+                                    fontSize = 22.sp
+                                )
+                            }
+                        }
+                        Text(
+                            text = "Month",
+                            color = colorResource(R.color.textColor),
+                            textAlign = TextAlign.Center
+                        )
+                        Card(
+                            backgroundColor = colorResource(R.color.cardBackground),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 12.dp)
+                                    .wrapContentWidth(Alignment.CenterHorizontally)
+                            ) {
+                                Text(
+                                    text = "Limit 12789, Left: 8234",
+                                    color = colorResource(R.color.textColor),
+                                    fontSize = 18.sp
+                                )
+                                IconButton(onClick = { }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Edit,
+                                        contentDescription = "Edit",
+                                        modifier = Modifier.size(20.dp),
+                                        tint = colorResource(R.color.textColor)
+                                    )
+                                }
+                            }
+                        }
+                        Text(
+                            text = "Expenses",
+                            color = colorResource(R.color.textColor),
+                            textAlign = TextAlign.Center
+                        )
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            items(listItems) { item ->
+                                Text(text = item, color = colorResource(id = R.color.textColor))
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return binding.root
 
     }
@@ -32,9 +130,9 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/dev/prem/newbudgetwidget/MainActivity.kt
+++ app/src/main/java/dev/prem/newbudgetwidget/MainActivity.kt
@@ -1,8 +1,6 @@
 package dev.prem.newbudgetwidget
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
@@ -37,21 +35,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        return when (item.itemId) {
+//            R.id.action_settings -> true
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)

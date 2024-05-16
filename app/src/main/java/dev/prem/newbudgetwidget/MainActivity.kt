ab4e80a42b428880b319ff1fdb0bdf06bda7922f
Premsuraj <premsrj@gmail.com>
2024-05-16 17:25:59 +0530

    Completed UI for AddEditActivity

--- app/src/main/java/dev/prem/newbudgetwidget/MainActivity.kt
+++ app/src/main/java/dev/prem/newbudgetwidget/MainActivity.kt
@@ -1,19 +1,15 @@
 package dev.prem.newbudgetwidget
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dev.prem.newbudgetwidget.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,26 +21,18 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            val navController = findNavController(R.id.nav_host_fragment_content_main)
-            navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAnchorView(R.id.fab)
-//                .setAction("Action", null).show()
+        binding.fab.setOnClickListener {
+            val addIntent = Intent(this, AddEditActivity::class.java)
+            startActivity(addIntent)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 
     fun getFloatingActionButton(): FloatingActionButton {
         return binding.fab
     }
+
+//    override fun onResume() {
+//        super.onResume()
+//        binding.toolbar.title = "Monthly Budget"
+//    }
 }
